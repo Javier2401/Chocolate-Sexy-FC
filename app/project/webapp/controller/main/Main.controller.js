c3cc4775f4ef01ac97c5ad40966cfd547f8e5d74
Javier <paltanlobeidajavier@iesblasdeotero.com>
2026-05-07 18:02:49 +0200

    Main corregido

--- app/project/webapp/controller/main/Main.controller.js
+++ app/project/webapp/controller/main/Main.controller.js
@@ -1,10 +1,13 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
+], function (Controller, MessageToast) {
     "use strict";
 
     return Controller.extend("chocolatesexy.project.controller.main.Main", {
-        onInit() {
-        }
+
+        onInit: function () {},
+
+
     });
 });
