559fabd5f202c849252aa423bc71b2c86474ad5f
Javier <paltanlobeidajavier@iesblasdeotero.com>
2026-05-10 15:58:19 +0200

    Prueba API instagram live update

--- app/project/webapp/controller/main/Main.controller.js
+++ app/project/webapp/controller/main/Main.controller.js
@@ -13,8 +13,8 @@ sap.ui.define([
                 'data-instgrm-captioned ' +
                 'data-instgrm-permalink="https://www.instagram.com/p/DXmsUdYjZFS/?utm_source=ig_embed&amp;utm_campaign=loading" ' +
                 'data-instgrm-version="14" ' +
-                'style="background:#FFF;border:0;border-radius:3px;' +
-                'box-shadow:0 0 1px 0 rgba(0,0,0,.5),0 1px 10px 0 rgba(0,0,0,.15);' +
+                'style="background:#FFF;border:0;border-radius:0;' +
+                'box-shadow:none;' +
                 'margin:1px;max-width:540px;min-width:326px;padding:0;width:99.375%;">' +
 
                 '<div style="padding:16px;">' +
