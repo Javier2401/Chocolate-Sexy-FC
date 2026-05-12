b4b7ca9a09b5800d63c908db1feac4bb428d6174
Javier <paltanlobeidajavier@iesblasdeotero.com>
2026-05-12 20:41:33 +0200

    Clasificación y calendario acabado

--- app/project/webapp/controller/pages/Leaderboard.controller.js
+++ app/project/webapp/controller/pages/Leaderboard.controller.js
@@ -6,8 +6,26 @@ sap.ui.define([
     return BaseController.extend("chocolatesexy.project.controller.pages.Leaderboard", {
 
         onInit: function () {
-            
+
         },
+
+        onToggleSeasonBlock: function(oEvent) {
+            var oButton      = oEvent.getSource();
+            var oSeasonBlock = oButton.getParent().getParent();
+            var oContent     = oSeasonBlock.getItems()[1];
+            var oDomRef      = oContent.getDomRef();
+
+            if (!oDomRef) return;
+
+            var bCollapsed = oDomRef.classList.contains("cal-collapsed");
+            oDomRef.classList.toggle("cal-collapsed");
+
+            oButton.setIcon(
+                bCollapsed
+                    ? "sap-icon://navigation-up-arrow"
+                    : "sap-icon://navigation-down-arrow"
+            );
+        }
 
     });
 });
