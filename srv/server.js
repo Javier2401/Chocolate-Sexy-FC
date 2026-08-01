0f99d8915bf64305e2cdb55b96b6f3fd07580c15
Javier <paltanlobeidajavier@iesblasdeotero.com>
2026-08-01 22:58:41 +0200

    Prueba 9.0

--- srv/server.js
+++ srv/server.js
@@ -1,7 +1,3 @@
 const cds = require('@sap/cds');
 
-cds.on('bootstrap', app => {
-  app.get('/', (req, res) => res.redirect('/chocolatesexy.project/index.html'));
-});
-
 module.exports = cds.server;
