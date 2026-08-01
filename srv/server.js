808f37bfd0b7e7246ebda4cf9a04053225093bab
Javier <paltanlobeidajavier@iesblasdeotero.com>
2026-08-01 23:01:50 +0200

    Prueba 10.0

--- srv/server.js
+++ srv/server.js
@@ -1,3 +1,10 @@
 const cds = require('@sap/cds');
 
+cds.on('bootstrap', app => {
+  app.get('/', (req, res, next) => {
+    req.url = '/index.html';
+    next();
+  });
+});
+
 module.exports = cds.server;
